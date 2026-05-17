8bddc6ed9774b988db33facb53281f699ceaf5a0
ramgolladi1503-sys <ramgolladi1503@gmail.com>
2026-05-17 17:22:02 +0530

    PR 63: export VWAP and trap providers

--- movement_engine/providers/__init__.py
+++ movement_engine/providers/__init__.py
@@ -19,17 +19,31 @@ from movement_engine.providers.opening_drive_orb import (
     orb_retest_provider,
     register_opening_drive_orb_providers,
 )
+from movement_engine.providers.vwap_trap import (
+    FAILED_BREAKOUT_TRAP_PROVIDER_ID,
+    VWAP_RECLAIM_PROVIDER_ID,
+    build_vwap_trap_candidate_pool,
+    failed_breakout_trap_provider,
+    register_vwap_trap_providers,
+    vwap_reclaim_provider,
+)
 
 __all__ = [
     "COMPRESSION_BREAKOUT_PROVIDER_ID",
+    "FAILED_BREAKOUT_TRAP_PROVIDER_ID",
     "OPENING_DRIVE_PROVIDER_ID",
     "ORB_RETEST_PROVIDER_ID",
     "TREND_PULLBACK_PROVIDER_ID",
+    "VWAP_RECLAIM_PROVIDER_ID",
     "build_compression_trend_candidate_pool",
+    "build_vwap_trap_candidate_pool",
     "compression_breakout_provider",
+    "failed_breakout_trap_provider",
     "opening_drive_provider",
     "orb_retest_provider",
     "register_compression_trend_providers",
     "register_opening_drive_orb_providers",
+    "register_vwap_trap_providers",
     "trend_pullback_provider",
+    "vwap_reclaim_provider",
 ]
